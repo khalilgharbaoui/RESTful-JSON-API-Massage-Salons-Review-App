31568a7093a74d1875d02897dc22a562340f3c89
khalilgharbaoui <kaygeee@gmail.com>
2016-02-28 14:48:31 +0100

    Rspec says 'massage salons have_many reviews' the model passes the test (TDD)

--- app/models/massagesalon.rb
+++ app/models/massagesalon.rb
@@ -1,3 +1,8 @@
 class Massagesalon < ActiveRecord::Base
-  validates :massage_salon_name, :massage_salon_city, :massage_salon_description, presence:true
+  has_many :reviews, dependent: :destroy
+validates :massage_salon_name, :massage_salon_city, :massage_salon_description, presence:true
+  # validates_presence_of :massage_salon_name, :message => "Massagesalon Name Can not be empty!"
+  # validates_presence_of :massage_salon_city, :message => "Massagesalon Name City not be empty!"
+  # validates_presence_of :massage_salon_description, :message => "Massagesalon Description Can not be empty!"
+
 end
--- spec/models/massagesalon_spec.rb
+++ spec/models/massagesalon_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Massagesalon, type: :model do
   it {is_expected.to validate_presence_of(:massage_salon_name && :massage_salon_city && :massage_salon_description) }
+  it {is_expected.to have_many(:reviews)}
 end
