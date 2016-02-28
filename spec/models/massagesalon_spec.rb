1872b90e71a622c13a3629a947399d35cc0bb5d5
khalilgharbaoui <kaygeee@gmail.com>
2016-02-28 14:15:03 +0100

    added the first model Rspec test (TDD)

--- spec/models/massagesalon_spec.rb
+++ spec/models/massagesalon_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Massagesalon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {is_expected.to validate_presence_of(:massage_salon_name) }
 end
