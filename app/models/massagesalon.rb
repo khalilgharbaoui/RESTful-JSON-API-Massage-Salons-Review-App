dbf5bb8209a6e819a1aed89ced03d14e6aabea0e
khalilgharbaoui <kaygeee@gmail.com>
2016-03-03 16:59:47 +0100

    heroookuu

--- app/models/massagesalon.rb
+++ app/models/massagesalon.rb
@@ -2,7 +2,7 @@ class Massagesalon < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :massage_salon_name, :massage_salon_city, :massage_salon_description, presence:true
   # validates_presence_of :massage_salon_name, :message => "Massagesalon Name Can not be empty!"
-  # validates_presence_of :massage_salon_city, :message => "Massagesalon Name City not be empty!"
+  # validates_presence_of :massage_salon_city, :message => "Massagesalon Name City not be empty !"
   # validates_presence_of :massage_salon_description, :message => "Massagesalon Description Can not be empty!"
 
 def average_rating
